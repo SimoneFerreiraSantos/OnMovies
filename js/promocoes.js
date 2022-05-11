8525a92e3eff1d71c3540a2aca267a6ae75f5e3f
Mariana Costa <marianaluizaac@gmail.com>
2022-05-11 14:42:22 -0300

    Pág. Promoçoes

--- js/promocoes.js
+++ js/promocoes.js
@@ -1,5 +1,4 @@
 
-console.log("Testando Js")
 var videos=[
     ["Up,Altas Aventuras","aventura.jpeg","2009","Aventura","Carl Fredricksen é um vendedor de balões que, aos 78 anos, está prestes a perder a casa em que sempre viveu com sua esposa, a falecida Ellie."],
     ["De Repente 30","romance.jpeg","2004","Romance","Jenna Rink é uma garota que está descontente com sua própria idade. A única amizade que possui é Matt, seu vizinho. Em seu 13º aniversário, ela faz um pedido: virar adulta."],
@@ -8,26 +7,24 @@ var videos=[
     ["Uma Noite no Museu 2","aventura2.jpeg","2009","Aventura","O guarda-noturno do Museu de História Natural Larry Daley é agora um empresário bem sucedido."],
     ["A Hora do Pesadelo","terror.jpeg","1984","Terror","Um grupo de adolescentes tem pesadelos horríveis, em que são atacados por um homem deformado com garras de aço. Ele apenas aparece durante o sono e, para escapar, é preciso acordar."]
 ]
-
-
-console.log(videos)
-
 var galeria=document.querySelector("#galeria")
-//logica de iteração papra consumo do array
 for(var i=0;i<videos.length;i++){
-        //cria um elemento div
         var div=document.createElement('div')
-        //adiciona a classe com espaço 6
         div.classList.add('col-sm-4')
         div.classList.add('mt-3')
-        //cria elemento div para o conteudo
+
         var conteudo=document.createElement('div')
         conteudo.className='row'
+
+        var ano='<p>' + videos[i][2] + '</p>'
+        var categoria='<p>' + videos[i][3] + '</p>'
+        var sinopse='<p>' + (videos[i][4]).substring(0,80) + '...</p>'
+        var descricao='<div class="col-sm-5 p-0">' + ano + categoria + sinopse + '</div>'
     conteudo.innerHTML+='<h3 class="col-sm-12 mt-4 mb-5">' + videos[i][0] + '</h3>'
     conteudo.innerHTML+=
-    '<div class="col-sm-4"><img src="imagens/' + videos[i][1]+'"class="w-100"/></div>'
+    '<div class="col-sm-7"><img src="imagens/' + videos[i][1]+'"class="w-100"/></div>'
 
-    conteudo.innerHTML+='<div class="col-sm-8"><p>'+ videos[i][2] +'</p><p>' + videos[i][3] + '</p><p>' + videos[i][4] + '</p></div>'
+    conteudo.innerHTML+=descricao
 
         div.appendChild(conteudo)
         galeria.appendChild(div)

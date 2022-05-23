167bc9357f5399b1e8735bcbdc93b09937f496d2
SimoneFerreiraSantos <simoneferreira2202@gmail.com>
2022-05-23 13:36:47 -0300

    Merge branch 'main' of https://github.com/SimoneFerreiraSantos/OnMovies into main

--- js/contato.js
+++ js/contato.js
@@ -1,4 +1,4 @@
-
+var chamado=document.querySelector('#chamado')
 var formC=document.forms.formContato
 document.querySelector("#btContato").onclick=function(){
     if(formC.nomeContato.value== ""){
@@ -8,10 +8,17 @@ document.querySelector("#btContato").onclick=function(){
     }else if (!formC.emailContato.value.includes('.', '@'))
     {
         alert("Preencha um e-mail válido");
+    }else if(formC.telefoneContato.value==""){
+        alert("Preencha o telefone!")
     }else if(formC.assunto.value==""){
         alert("Preencha o assunto!")
     }else if(formC.comentario.value==""){
         alert("Preencha o comentário!")
+    }else if(chamado.checked==true){
+        let idChamado=parseInt(Math.random()* 90000)+10000 
+        let contatoChamado=formC.nomeContato.value + ", " + formC.emailContato.value + ", " + formC.telefoneContato.value + ", " + formC.assunto.value
+        localStorage.setItem('chamado-' + idChamado , contatoChamado )
+        formC.submit()
     }else{
         formC.submit()
     }

561dc359c48a89f98ffb22b79feab89d86f41a00
Gisele <gisesonia@gmail.com>
2022-06-01 16:42:56 -0300

    lógica do javascript para adicionar imagens

--- js/index.js
+++ js/index.js
@@ -27,38 +27,50 @@ localStorage.emailUser ? msgUser() : resetUser()
 
 let srcImg = ""
 let nameImg = ""
+let itemsArray = []
+let outroArray = []
+let divRodape= document.querySelector('#rodapeImg')
+let novoArray = []
+let divCol = ''
 
 var getAllImages = document.getElementById('divImagens').getElementsByTagName('img'); 
-    for (var i = 0; i < getAllImages.length; i++) { (function(x) { 
-     getAllImages[x].addEventListener('click', function() { srcImg = this.getAttribute('src')
-     nameImg=this.getAttribute('name')
-     localStorage.setItem("titulo-" + nameImg,srcImg)
-        }) }(i)) 
-    }
+
+document.querySelectorAll('.img-thumbnail').forEach(item => {
+    item.addEventListener('click', event => {
+        console.log("teste")
+        console.log(event.currentTarget.getAttribute('src'))
+        srcImg = item.getAttribute('src')
+        nameImg= item.getAttribute('name')
+        console.log(srcImg)
+        localStorage.setItem("titulo-" + nameImg,srcImg)
+    })
+  })
+    
 
 let recuperaFilmesArray = []
-for(let i=0; i<localStorage.length;i++){
-    if(localStorage.key(i).includes("titulo")){
-        let recuperaFilmes = localStorage.key(i) + "," + localStorage.getItem(localStorage.key(i)) 
-        recuperaFilmesArray.push(recuperaFilmes.split(','))
+for(let k=0; k<localStorage.length;k++){
+    if(localStorage.key(k).includes("titulo")){
+        let recuperaFilmes = localStorage.key(k) + "," + localStorage.getItem(localStorage.key(k)) 
+        recuperaFilmesArray.unshift(recuperaFilmes.split(','))        
     }
 }
-if (recuperaFilmesArray.length > 2 ){
-    let divRodape= document.querySelector('#rodapeImg')
-    console.log(divRodape.childNodes)
-    divRodape.innerHTML=""
 
-for(let y=0; y < 3 ;y++){
-    let divRodape= document.querySelector('#rodapeImg')
-    let img = '<div class="col-sm-4"><a href="docs/singlePreferencia.html"><img src="' + recuperaFilmesArray[y][1] + '" class="img-thumbnail"></a></div>'
-    
-    divRodape.innerHTML+=img
-    }
+console.log(recuperaFilmesArray)
+
+novoArray = recuperaFilmesArray.slice(-3)
+
+console.log(novoArray)
+
+for (let index = 0; index < novoArray.length; index++) {
+    divCol = document.createElement("div")
+    divCol.className = "col-sm-4"
+
+
+    let divImg = document.createElement("img")
+    divImg.className = "img-thumbnail w-50"
+    divImg.src = novoArray[index][1]
+
+    divCol.appendChild(divImg)
+
+    divRodape.children.length < 3 ? divRodape.append(divCol) :  divRodape.removeChild(divCol)
 }
-var srcImgUnica=""
-var getImages = document.getElementById('rodapeImg').getElementsByTagName('img'); 
-    for (var i = 0; i < getImages.length; i++) { (function(x) { 
-     getImages[x].addEventListener('click', function() { srcImgUnica = this.getAttribute('src')
-     localStorage.setItem("filme",srcImgUnica)
-        }) }(i)) 
-    }

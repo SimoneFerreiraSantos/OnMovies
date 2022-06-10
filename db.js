c418ee07aca8e2dabc212fbc8e6231ff2d7d253a
impulsoteck2 <impulsoteck2@gmail>
2022-06-10 14:59:22 -0500

    inclusão insertFilmes e rota post

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ async function conecta(){
     const mysql = require("mysql2/promise")
     const conn = await mysql.createConnection({
         host: "localhost",
-        user: "root",
-        password: "Roque123$#@!",
+        user: "r14",
+        password: "Re14$$",
         database:"projeto_video"
     })
     console.log("mySQL conectado!")
@@ -37,6 +37,13 @@ async function updatePromocoes(promo,id){
     return await conectado.query("UPDATE filmes SET promo=? WHERE filmes_id=?", values)
    
 }
+async function insertFilmes(filme){
+    const conectado = await conecta()
+    const values = [filme.titulo, filme.categoria,filme.ano,filme.sinopse,filme.imagem]
+    return await conectado.query("INSERT INTO filmes (titulo,categora,ano,sinopse,imagem) VALUES(?,?,?,?,?)", values)
+   
+}
+
 //selectFilmes()
 
-module.exports = {selectFilmes,selectSingle,selectPromocoes,updatePromocoes}
+module.exports = {selectFilmes,selectSingle,selectPromocoes,updatePromocoes,insertFilmes}

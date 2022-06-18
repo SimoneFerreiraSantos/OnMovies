07c10ec6adef4e7691b1b24a71a243f7779c7ce7
luana-gruber <luana_gruber@hotmail.com>
2022-06-18 16:05:44 -0300

    Alteração nav e header usuário e adm

--- db.js
+++ db.js
@@ -5,8 +5,8 @@ async function conecta(){
     const mysql = require("mysql2/promise")
     const conn = await mysql.createConnection({
         host: "localhost",
-        user: "ln002",
-        password: "Lb21@$#!",
+        user: "l42",
+        password: "Usuario@1992",
         database:"projeto_video"
     })
     console.log("mySQL conectado!")
